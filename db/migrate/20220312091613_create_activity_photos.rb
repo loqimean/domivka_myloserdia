8459d9cb1307c38a3734efe288ac49c26ab256de
vasyl20 <d836650@gmail.com>
2022-03-12 14:53:10 +0200

    migration updates

--- db/migrate/20220312091613_create_activity_photos.rb
+++ db/migrate/20220312091613_create_activity_photos.rb
@@ -1,7 +1,7 @@
 class CreateActivityPhotos < ActiveRecord::Migration[7.0]
   def change
     create_table :activity_photos do |t|
-      t.string :photo
+      t.string :photo, null: false
 
       t.timestamps
     end
