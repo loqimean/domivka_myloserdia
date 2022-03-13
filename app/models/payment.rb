f6633c3c717c3508f7ac9abdd921e80189babb35
vasyl20 <d836650@gmail.com>
2022-03-13 16:52:38 +0200

    add validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,5 @@
 class Payment < ApplicationRecord
-    mount_uploader :sum, PaymentUploader, presence: true
-    mount_uploader :name, PaymentUploader, presence: true
-
     scope :ordered, -> { order(:created_at) }
+    
+    validates :sum, :name, presence: true
 end
