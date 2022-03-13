e9f3cf08636e234d36185bdc4398328358a98e92
vasyl20 <d836650@gmail.com>
2022-03-13 17:08:01 +0200

    add rspec

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:sum) }
+    it { should validate_presence_of(:name) }
+  end
 end
