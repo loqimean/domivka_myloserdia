dc58f249f39faca8e3e8da17b8c50660d41ae684
vasyl20 <d836650@gmail.com>
2022-03-13 17:58:51 +0200

    add model Invoice

---
+++ db/migrate/20220313153000_create_invoices.rb
@@ -0,0 +1,9 @@
+class CreateInvoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.string :file,    null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/invoices.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :invoice do
+    file { "MyString" }
+  end
+end
