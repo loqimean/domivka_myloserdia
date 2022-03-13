2131bf75efb1ab1d64cc8a2d99187f50c719db2f
vasyl20 <d836650@gmail.com>
2022-03-13 18:16:16 +0200

    update migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_171001) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_13_153000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,9 +20,29 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_171001) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "invoices", force: :cascade do |t|
+    t.string "file", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "payments", force: :cascade do |t|
     t.string "sum"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "site_settings", force: :cascade do |t|
+    t.text "uk_idea_of_project"
+    t.text "en_idea_of_project"
+    t.string "main_photo"
+    t.text "adress"
+    t.string "payment_details"
+    t.string "email"
+    t.string "facebook_url"
+    t.string "instagram_url"
+    t.string "telegram_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
