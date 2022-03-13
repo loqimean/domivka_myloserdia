49c799b575bd40731b5a12fc22d5e7f25ec66cfe
vasyl20 <d836650@gmail.com>
2022-03-13 15:50:55 +0200

    update ActivityPhoto, SituationPhoto

--- app/models/activity_photo.rb
+++ app/models/activity_photo.rb
@@ -1,5 +1,7 @@
 class ActivityPhoto < ApplicationRecord
     mount_uploader :photo, ActivityPhotoUploader
+
+    validates :photo, presence: true
     
     scope :ordered, -> { order(:created_at) }
 end
--- app/models/situation_photo.rb
+++ app/models/situation_photo.rb
@@ -1,5 +1,7 @@
 class SituationPhoto < ApplicationRecord
     mount_uploader :photo, SituationPhotoUploader
+    
+    validates :photo, presence: true
 
     scope :ordered, -> { order(:created_at) }
 end
