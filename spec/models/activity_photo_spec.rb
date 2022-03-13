2229893333cfee3999d929fc0ebeb33064d7ae40
vasyl20 <d836650@gmail.com>
2022-03-13 23:31:49 +0200

    update tailwind, activity_photo_spec

--- spec/models/activity_photo_spec.rb
+++ spec/models/activity_photo_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ActivityPhoto, type: :model do
-    describe 'validations' do
-      it { is_expected.to validate_presence_of(:photo) }
-    end 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:photo) }
+  end 
 end
