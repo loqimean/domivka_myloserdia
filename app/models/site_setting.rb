cbd478b402c195e784123dbd09bd73ac773186da
loqimean <vanuha277@gmail.com>
2022-03-20 10:46:48 +0200

    feat(views|models) make SiteSettings more setables

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -4,6 +4,8 @@ class SiteSetting < ApplicationRecord
   validates :uk_idea_of_project, :en_idea_of_project, length: { minimum: 50 }
   validates :main_photo, :uk_idea_of_project, :en_idea_of_project, presence: true
 
+  has_rich_text :uk_idea_of_project
+  has_rich_text :en_idea_of_project
   has_rich_text :uk_payment_details
   has_rich_text :en_payment_details
   has_rich_text :uk_address
